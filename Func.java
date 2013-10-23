d43e8786f66ab95b7924256bb78e4d3bd1ab939b
priyanmuthu <priyanmuthu@gmail.com>
2013-10-23 23:07:39 +0530

    Create Func.java

---
+++ Func.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cims;
+
+/**
+ *
+ * @author Priyan
+ */
+public class Func {
+    static String monthCast(String mon){
+        switch (mon) {
+            case "January":
+                return "01";
+            case "February":
+                return "02";
+            case "March":
+                return "03";
+            case "April":
+                return "04";
+            case "May":
+                return "05";
+            case "June":
+                return "06";
+            case "July":
+                return "07";
+            case "August":
+                return "08";
+            case "September":
+                return "09";
+            case "October":
+                return "10";
+            case "November":
+                return "11";
+            case "December":
+                return "12";
+        }
+        return null;
+    }
+    
+    static String monthCastInvert(String mon){
+        switch (mon) {
+            case "01":
+                return "January";
+            case "02":
+                return "February";
+            case "03":
+                return "March";
+            case "04":
+                return "April";
+            case "05":
+                return "May";
+            case "06":
+                return "June";
+            case "07":
+                return "July";
+            case "08":
+                return "August";
+            case "09":
+                return "September";
+            case "10":
+                return "October";
+            case "11":
+                return "November";
+            case "12":
+                return "December";
+        }
+        return null;
+    }
+}
