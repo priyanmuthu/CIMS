17ea1e3ab6d840353dba9a423e773f2773b78ea7
priyanmuthu <priyanmuthu@gmail.com>
2013-10-23 23:04:15 +0530

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cims;
+
+/**
+ *
+ * @author Priyan
+ */
+final public class Constant {
+    static String database_name = "cims";
+    static String chemical_table = "chemicals";
+    static String user_table = "users";
+    static String inventory_table = "inventory";
+    static String borrow_table = "borrow";
+    static String borrow_log_table = "borrowlog";
+    static String inventory_log_table = "inventorylog";
+}
